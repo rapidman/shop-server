f0657cbea5307c113f7920bef44f8e8245535113
Timur Saidov <t.saidov@fasten.com>
2018-05-02 17:31:46 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+1. Setup
+ - download project https://start.spring.io/
+ - https://developer.okta.com/blog/2017/12/04/basic-crud-angular-and-spring-boot
---
+++ src/main/java/com/ajax/shop/entity/Car.java
@@ -0,0 +1,17 @@
+package com.ajax.shop.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@ToString @EqualsAndHashCode
+public class Car {
+    @Id @GeneratedValue
+    private Long id;
+    private @NonNull String name;
+}
